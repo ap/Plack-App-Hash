3f91defe560987eb3d92a1d5ed1828441d416b8c
Aristotle Pagaltzis <pagaltzis@gmx.de>
2015-01-10 10:16:55 +0100

    rewrite for clarity and sometimes speed

--- lib/Plack/App/Hash.pm
+++ lib/Plack/App/Hash.pm
@@ -26,36 +26,40 @@ sub call {
 	return $self->error( 404 ) unless $content and exists $content->{ $path };
 	return $self->error( 500 ) if ref $content->{ $path };
 
-	my $headers = $self->headers;
-	my $hdrs = ( $headers and exists $headers->{ $path } ) ? $headers->{ $path } : [];
-	if ( not ref $hdrs ) {
-		require JSON::XS;
-		$hdrs = JSON::XS::decode_json $hdrs;
+	my $headers = ( $self->headers || $self->headers( {} ) )->{ $path };
+
+	if ( not defined $headers ) {
+		$headers = [];
 	}
+	elsif ( not ref $headers ) {
+		require JSON::XS;
+		$headers = JSON::XS::decode_json $headers;
+	}
+
+	return $self->error( 500 ) if 'ARRAY' ne ref $headers;
 
 	{
 		my $auto    = $self->auto_type;
 		my $default = $self->default_type;
 		last unless $auto or $default;
-		last if Plack::Util::header_exists $hdrs, 'Content-Type';
+		last if Plack::Util::header_exists $headers, 'Content-Type';
 		$auto &&= do { require Plack::MIME; Plack::MIME->mime_type( $path ) };
-		my $type = $auto || $default;
-		Plack::Util::header_push $hdrs, 'Content-Type' => $type if $type;
+		Plack::Util::header_push $headers, 'Content-Type' => $_ for $auto || $default || ();
 	}
 
-	if ( not Plack::Util::header_exists $hdrs, 'Content-Length' ) {
-		Plack::Util::header_push $hdrs, 'Content-Length' => length $content->{ $path };
+	if ( not Plack::Util::header_exists $headers, 'Content-Length' ) {
+		Plack::Util::header_push $headers, 'Content-Length' => length $content->{ $path };
 	}
 
-	my $body = [];
-	Array::RefElem::av_push @$body, $content->{ $path };
-	return [ 200, $hdrs, $body ];
+	my @body;
+	Array::RefElem::av_push @body, $content->{ $path };
+	return [ 200, $headers, \@body ];
 }
 
 sub error {
 	my $status = pop;
 	my $pkg = __PACKAGE__;
-	my $body = [ qq(<!doctype html>\n<title>$pkg $status</title><h1><font face=sans-serif>) . HTTP::Status::status_message $status ];
+	my $body = [ qq(<!DOCTYPE html>\n<title>$pkg $status</title><h1><font face=sans-serif>) . HTTP::Status::status_message $status ];
 	return [ $status, [
 		'Content-Type'   => 'text/html',
 		'Content-Length' => length $body->[0],
